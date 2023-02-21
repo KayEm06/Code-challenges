d6d510eae83174b244cecf1ec6e61712921a6567
Kuba <62169414+KayEm06@users.noreply.github.com>
2023-02-21 21:34:29 +0000

    Add files via upload

---
+++ Problem 19.py
@@ -0,0 +1,25 @@
+def logicgates(gate, first, second):
+    result = None
+    if gate == "or":
+        result = first or second
+    elif gate == "and":
+        result = first and second
+    elif gate == "xor":
+        if (first == 0 and second == 1) or (first == 1 and second == 0):
+            result = first or second
+        result = 0
+    elif gate == "nand":
+        result = not(first and second)
+    elif gate == "nor":
+        result = not(first or second)
+    else:
+        print("Invalid input")
+    return result
+
+gate = input("Enter logic gate: ")
+gate = gate.lower()
+first = int(input("Enter the first input: "))
+second = int(input("Enter the second input: "))
+
+result = logicgates(gate, first, second)
+print(f"Result = {result}")
